b8d5373856653c7f943e318efd581e8df0247424
Luke Griffiths <wlgriffiths@gmail.com>
2013-06-19 18:18:20 -0400

    added devise

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module CurrencyTracker
   class Application < Rails::Application
+    config.assets.initilize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
